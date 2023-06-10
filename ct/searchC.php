b03cb1dc9ab1b4a86884c9066a98cc1fc9cec461
HubertHuckevoll <konstantin.meyer@gmail.com>
2023-06-10 18:19:54 +0200

    creating search

---
+++ ct/searchC.php
@@ -0,0 +1,6 @@
+<?php
+
+class searchC extends cbmSearchC
+{}
+
+?>
---
+++ vw/php/searchV.php
@@ -0,0 +1,18 @@
+<?php
+
+class searchV extends cbmPageV
+{
+  /**
+   * Summary of drawRandom
+   * @return string
+   * ________________________________________________________________
+   */
+  public function cbmContent(): string
+  {
+    logger::vh($this->data);
+    return '';
+  }
+
+}
+
+?>
