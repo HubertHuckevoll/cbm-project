7769bbed33b1d6ecc6f555f1e7bbebe86cd56cd4
HubertHuckevoll <konstantin.meyer@gmail.com>
2023-06-16 21:42:24 +0200

    further tests

--- xml.php
+++ xml.php
@@ -33,23 +33,28 @@ $hstr = <<<XML
     <title>Schneeammer</title>
     <header>Schneeammer</header>
     <summary>Schneeammer</summary>
-    <content xmlns="http://www.w3.org/1999/xhtml">
-    <ul>
-        <li>
-        Ab Mitte der 1770er Jahre ließ der auf Gordon Castle residierende Alexander Gordon, 4. Duke of Gordon die Ortschaft Fochabers in
-        größerer Entfernung zu seinem Sitz als Plansiedlung neu aufbauen.[2] Die Bellie Kirk sollte hierbei das Herzstück der neuen Ortschaft
-        bilden. Mit der Planung des Kirchengebäudes wurde John Baxter betraut. Die Bellie Kirk entstand zwischen 1795 und 1798 nach seinen
-        Entwürfen. In den 1840er Jahren wurde der Innenraum des Gebäudes überarbeitet und neu arrangiert. Letztere Änderungen wurde später
-        wieder zurückgenommen.
-        </li>
-        <li>
-        1947 verschmolz die Kirchengemeinde mit der Gemeinde der Pringle Church der United Free Church. Rückwärtig entstand 1985 ein neuer
-        Gemeindesaal. 1998, ein Jahr nach der 200-Jahrfeier, erhielt die Apsis neue Bleiglasfenster. Eine neue Orgel wurde im Jahre 2007
-        installiert. Sie ersetzte ein rund 120 Jahre altes Instrument. 2009 wurde die seit mehreren Jahren andauernde Modernisierung des
-        Innenraums abgeschlossen. 2015 verschmolz die Kirchengemeinde mit der Gemeinde Speymouth. Im Folgejahr wurden umfassende
-        Instandsetzungsarbeiten am Glockenturm abgeschlossen.
-        </li>
-    </ul>
+    <content>
+        <div xmlns="http://www.w3.org/1999/xhtml">
+            <ul>
+                <li>
+                Ab Mitte der 1770er Jahre ließ der auf Gordon Castle residierende Alexander Gordon, 4. Duke of Gordon die Ortschaft Fochabers in
+                größerer Entfernung zu seinem Sitz als Plansiedlung neu aufbauen.[2] Die Bellie Kirk sollte hierbei das Herzstück der neuen Ortschaft
+                bilden. Mit der Planung des Kirchengebäudes wurde John Baxter betraut. Die Bellie Kirk entstand zwischen 1795 und 1798 nach seinen
+                Entwürfen. In den 1840er Jahren wurde der Innenraum des Gebäudes überarbeitet und neu arrangiert. Letztere Änderungen wurde später
+                wieder zurückgenommen.
+                </li>
+                <li>
+                1947 verschmolz die Kirchengemeinde mit der Gemeinde der Pringle Church der United Free Church. Rückwärtig entstand 1985 ein neuer
+                Gemeindesaal. 1998, ein Jahr nach der 200-Jahrfeier, erhielt die Apsis neue Bleiglasfenster. Eine neue Orgel wurde im Jahre 2007
+                installiert. Sie ersetzte ein rund 120 Jahre altes Instrument. 2009 wurde die seit mehreren Jahren andauernde Modernisierung des
+                Innenraums abgeschlossen. 2015 verschmolz die Kirchengemeinde mit der Gemeinde Speymouth. Im Folgejahr wurden umfassende
+                Instandsetzungsarbeiten am Glockenturm abgeschlossen.
+                </li>
+            </ul>
+            <div>
+                Seattle <strong>Firefighters</strong>.
+            </div>
+        </div>
     </content>
     <images>
         <img title="Schneeammer">schneeammer.png</img>
@@ -59,12 +64,13 @@ XML;
 
 
 $xml = simplexml_load_string($hstr, null, LIBXML_NOCDATA | LIBXML_NOBLANKS);
-$con = json_encode($xml);
-$newArr = json_decode($con, true);
+$json = json_encode($xml);
+$arr = json_decode($json, true);
+$arr['content'] = $xml->xpath('/content/*')[0]->asXML();
 
 $out = '';
 $out  = '<pre>';
-$out .= print_r($newArr, true);
+$out .= print_r($arr, true);
 $out .= '</pre>';
 
 echo $out;
