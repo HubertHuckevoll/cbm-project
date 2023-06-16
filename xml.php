d26d7d2d725acfc3b669692b39ae09c355be85dc
HubertHuckevoll <konstantin.meyer@gmail.com>
2023-06-16 10:41:54 +0200

    further tests for XML

--- xml.php
+++ xml.php
@@ -7,11 +7,19 @@ $str = <<<XML
     <header>Schneeammer</header>
     <summary>Schneeammer</summary>
     <content>
-        <![CDATA[
-            <ul>
-                <li>Gesichtet am 15.03.2023 auf Wangerooge.</li>
-            </ul>
-        ]]>
+<![CDATA[
+Ab Mitte der 1770er Jahre ließ der auf Gordon Castle residierende Alexander Gordon, 4. Duke of Gordon die Ortschaft Fochabers in
+größerer Entfernung zu seinem Sitz als Plansiedlung neu aufbauen.[2] Die Bellie Kirk sollte hierbei das Herzstück der neuen Ortschaft
+bilden. Mit der Planung des Kirchengebäudes wurde John Baxter betraut. Die Bellie Kirk entstand zwischen 1795 und 1798 nach seinen
+Entwürfen. In den 1840er Jahren wurde der Innenraum des Gebäudes überarbeitet und neu arrangiert. Letztere Änderungen wurde später
+wieder zurückgenommen.
+
+1947 verschmolz die Kirchengemeinde mit der Gemeinde der Pringle Church der United Free Church. Rückwärtig entstand 1985 ein neuer
+Gemeindesaal. 1998, ein Jahr nach der 200-Jahrfeier, erhielt die Apsis neue Bleiglasfenster. Eine neue Orgel wurde im Jahre 2007
+installiert. Sie ersetzte ein rund 120 Jahre altes Instrument. 2009 wurde die seit mehreren Jahren andauernde Modernisierung des
+Innenraums abgeschlossen. 2015 verschmolz die Kirchengemeinde mit der Gemeinde Speymouth. Im Folgejahr wurden umfassende
+Instandsetzungsarbeiten am Glockenturm abgeschlossen.
+]]>
     </content>
     <images>
         <img title="Schneeammer">schneeammer.png</img>
@@ -23,6 +31,11 @@ $xml = simplexml_load_string($str, null, LIBXML_NOCDATA | LIBXML_NOBLANKS);
 $con = json_encode($xml);
 $newArr = json_decode($con, true);
 
-echo '<pre>'.print_r($newArr, true).'</pre>';
+$out = '';
+//$out  = '<pre>';
+$out .= print_r($newArr, true);
+//$out .= '</pre>';
+
+echo $out;
 
 ?>
