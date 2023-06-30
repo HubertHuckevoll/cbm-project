aaef41af1d5fc9c887e6cf50907d26d0d466e1fd
HubertHuckevoll <konstantin.meyer@gmail.com>
2023-06-30 11:11:07 +0200

    redirect after upload

--- ct/pwaC.php
+++ ct/pwaC.php
@@ -4,19 +4,21 @@ class pwaC extends cPageC
 {
   public function __construct(array $request, ?array $prefs = null)
   {
-    $view = new pwaV('pwaV');
-    parent::__construct($request, $prefs, $view);
+    parent::__construct($request, $prefs);
   }
 
   public function show()
   {
-    $this->view->drawPage('PWA Upload!', '', '');
+    $view = new pwaV('pwaV');
+    $view->drawPage('PWA Upload!', '', '');
   }
 
   public function upload()
   {
     $results = [];
     $upDir = "../cbm-project.data/entries.assets/";
+
+    //$str = print_r($_FILES + $this->request, true);
 
     foreach($_FILES as $file)
     {
@@ -27,9 +29,27 @@ class pwaC extends cPageC
       }
     }
 
-    $str = print_r($_FILES + $this->request, true);
-    $this->view->drawPage('PWA Upload!', $str, $results[0]);
+    $this->redirect($results[0]);
   }
+
+  public function create()
+  {
+    $view = new pwaV('pwaV');
+    $img = $this->request['img'];
+
+    $view->drawPage('PWA Upload!', 'Holla die Waldfee, it worked.', $img);
+  }
+
+  public function redirect($img)
+  {
+    $href = dirname($_SERVER['PHP_SELF']);
+    $href = substr($href, 0, strrpos($href, 'index.php'));
+    $href = rtrim($href, '/\\');
+    $href = 'https://'.$_SERVER['HTTP_HOST'].$href.'/index.php/pwaC/create?img='.$img;
+
+    header('Location: '.$href, null, 303);
+  }
+
 }
 
 ?>
