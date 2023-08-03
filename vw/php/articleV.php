9a676c2921a9b3cfc2afc2b1c11caa55ae50c90e
MeyerK <konstantin.meyer@gmail.com>
2023-08-03 21:51:06 +0200

    further separating data format from view

--- vw/php/articleV.php
+++ vw/php/articleV.php
@@ -4,9 +4,18 @@ class articleV extends cbmV
 {
   public function drawPage(array $index, array $article): void
   {
+    $metadata = [];
+    $image0 = $article['xml']->images->children()[0] ?? null;
+    $metadata['image0'] = ($image0 !== null) ? $this->makeAssetURL($article, $image0, true) : null;
+    $metadata['imageTitle'] = $article['xml']->images->children()[0]['title'] ?? '';
+    $metadata['summary'] = $article['xml']->summary ?? '';
+    $metadata['author'] = $article['xml']->author ?? $_SERVER['SERVER_NAME'];
+    $metadata['title'] = $article['xml']->title ?? '';
+    $metadata['date'] = $article['date'] ?? '';
+
     $this->setTag('base', $this->renderBaseTag());
     $this->setTag('title', $article['xml']->title);
-    $this->setTag('metadata', $this->renderArticleMetadata($article));
+    $this->setTag('metadata', $this->renderArticleMetadata($metadata));
     $this->setTag('date', $this->renderDate($article['date']));
     $this->setTag('article', $article['xml']->content);
     $this->setTag('images', $this->renderImageList($article, $index['tags']));
--- vw/php/galleryV.php
+++ vw/php/galleryV.php
@@ -4,9 +4,18 @@ class galleryV extends cbmV
 {
   public function drawPage(array $article, array $gallery, string $tags): void
   {
+    $metadata = [];
+    $image0 = $article['xml']->images->children()[0] ?? null;
+    $metadata['image0'] = ($image0 !== null) ? $this->makeAssetURL($article, $image0, true) : null;
+    $metadata['imageTitle'] = $article['xml']->images->children()[0]['title'] ?? '';
+    $metadata['summary'] = $article['xml']->summary ?? '';
+    $metadata['author'] = $article['xml']->author ?? $_SERVER['SERVER_NAME'];
+    $metadata['title'] = $article['xml']->title ?? '';
+    $metadata['date'] = $article['date'] ?? '';
+
     $this->setTag('base', $this->renderBaseTag());
     $this->setTag('title', $article['xml']->title);
-    $this->setTag('metadata', $this->renderArticleMetadata($article));
+    $this->setTag('metadata', $this->renderArticleMetadata($metadata));
     $this->setTag('content', $this->renderGallery($article, $gallery, $tags));
 
     $this->draw();
